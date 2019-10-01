816c1b4be46ccc9c716ed4d3f7885336b4d1b84b
Colleen McGuire <mzmacky2@gmail.com>
2019-10-01 14:46:41 -0400

    good work... see comment in code

--- Pig Latin.py
+++ Pig Latin.py
@@ -2,7 +2,7 @@
 #takes a user inputted word and translates it into Pig Latin
 myWord = input("Choose a word")
 myList = ["a","e","i","o","u"]
-if (myWord[0:1] in myList):
+if (myWord[0:1] in myList):  # only need to say myword[0]  not myword[0:1]
     print(myWord + "yay")
 else:
     print(myWord[1:] + myWord[0:1] +"ay")
